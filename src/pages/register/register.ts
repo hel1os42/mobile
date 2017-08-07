5a5676f24776a873ff35c9c1a2237728ffdcfcd9
serhiiporaiko <serhiiporaiko@gmail.com>
2017-08-07 12:55:29 +0300

    handled registration error

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Register } from "../../models/register";
 import { AuthService } from "../../providers/auth.service";
+import { ToastController } from "ionic-angular";
 
 @Component({
     selector: 'page-register',
@@ -11,19 +12,50 @@ export class RegisterPage {
     inviteCode: string = '59713'
     data: Register = new Register();    
 
-    constructor(private authService: AuthService) {
+    constructor(
+        private authService: AuthService,
+        private toast: ToastController) {
+
         this.authService
             .getReferrerId(this.inviteCode)
-            .subscribe(res => {
-                this.data = res.json().data;
-            });
+            .subscribe(
+                resp => {
+                    this.data = resp.json();
+                },
+                errResp => {
+                    debugger;
+                }
+            );
     }
 
     register() {
         this.authService
             .register(this.data)
-            .subscribe(res => {
-                console.log('User has been successfully registered', res.json());
-            });
+            .subscribe(
+                resp => {
+                    console.log('User has been successfully registered', resp.json());
+                },
+                errResp => {
+                    let err = errResp.json();
+                    let messages = [];
+
+                    for (let key in err) {
+                        let el = err[key];
+                        for (let i = 0; i < el.length; i++) {
+                            let msg = el[i];
+                            messages.push(msg);
+                        }
+                    }
+
+                    let toast = this.toast.create({
+                        message: messages.join('\n'),
+                        duration: 5000,
+                        position: 'bottom',
+                        dismissOnPageChange: true,
+                        cssClass: 'toast'
+                    });
+                    toast.present();
+                }
+            );
     }
 }
