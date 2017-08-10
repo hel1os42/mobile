58e7be0f3fd871c569d3a3d5945a5bc617fa88e6
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-08-10 13:54:58 +0300

    added user model

---
+++ src/models/user.ts
@@ -0,0 +1,15 @@
+export class User {
+    name: string;
+    points: number;
+    level: number;
+    referralUsers: any;
+    offers: any;
+    myOffers: any
+    email: string;
+    password: string;
+    referrer_id: string;
+    icon;
+    logo;
+    nau;
+
+}
