7f21a932683ef457da2e79d7efd0cf9da81e7941
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-08-14 14:46:36 +0300

    add data to UserPage

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -3,24 +3,27 @@ import { App } from 'ionic-angular';
 import { AuthService } from "../../providers/auth.service";
 import { StartPage } from "../start/start";
 import { ProfileService } from "../../providers/profile.service";
+import { User } from "../../models/user";
 
 @Component({
   selector: 'page-user',
   templateUrl: 'user.html'
 })
 export class UserPage {
+    data: User = new User();
 
   constructor(
     private app: App,
     private profile: ProfileService,
-    private auth: AuthService) {
+    private auth: AuthService,) {
 
   }
 
   ionViewDidEnter() {
       this.profile.get()
         .subscribe(profile => {
-            console.log(profile);
+            this.data = profile;
+            console.log(this.data);
         })
   }
 
