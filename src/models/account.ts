033d94dbe856026589c1d6ac50615747e71fd042
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-09-06 14:08:33 +0300

    added account model

---
+++ src/models/account.ts
@@ -0,0 +1,11 @@
+export class Account {
+    name: string;
+    email: string;
+    facebookName: string;
+    twitterName: string;
+    instagramName: string;
+    gender: string;
+    age: number;
+    income: number;
+    location: string;
+}
