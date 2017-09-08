f6abf3ee92973ef190dbb574cfc6601f308bbdd4
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-09-08 23:30:28 +0300

    connected API GET /offers

--- src/pages/user-offers/user-offers.ts
+++ src/pages/user-offers/user-offers.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RedeemedOffer } from '../../models/redeemedOffer';
+import { ProfileService } from '../../providers/profile.service';
 
 @Component({
   selector: 'page-user-offers',
   templateUrl: 'user-offers.html'
 })
 export class UserOffersPage {
+  offers: RedeemedOffer[];
+  total: number;
 
-  constructor(private nav: NavController) {
+  constructor(private nav: NavController,
+              private profile: ProfileService) {
 
   }
+
+  ionViewDidLoad() {
+    this.profile.getOffers()
+      .subscribe(resp => {            
+          this.offers = resp.data;
+          this.total = resp.total;
+    })
+}
 
 }
