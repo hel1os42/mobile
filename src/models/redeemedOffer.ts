74e8550443e00f8a15b7f0de8ba8484c858e25ba
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-09-13 15:35:03 +0300

    fixed redeemed offer model

--- src/models/redeemedOffer.ts
+++ src/models/redeemedOffer.ts
@@ -1,18 +1,31 @@
 export class RedeemedOffer {
-    
-        id: string;
-        country: string;
-        city: string;
-        radius: number;
-        account_id: string;
-        label: string;
-        description: string;
-        start_date: string;
-        finish_date: string;
-        start_time: string;
-        finish_time: string;
-        category_id: string;
-        user_level_min: number;
-        latitude;
-        longitude;
+    id: string;
+    reward: number;
+    status: string;
+    country: string;
+    city: string;
+    max_count: number;
+    max_for_user: number;
+    max_per_day: number;
+    max_for_user_per_day: number;
+    radius: number;
+    created_at: string;
+    updated_at: string;
+    account_id: number;
+    label: string;
+    description: string;
+    start_date: string;
+    finish_date: string;
+    start_time: string;
+    finish_time: string;
+    category_id: string;
+    user_level_min: number;
+    latitude: string;
+    longitude: string;
+    pivot: {
+        user_id: string;
+        offer_id: string;
+        created_at: string;
+        points: number;
     }
+}
