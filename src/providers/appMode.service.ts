54c879e0578834708e6e2262c82b96d2a0d93703
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-09-15 12:28:50 +0300

    added methods in app mode service

--- src/providers/appMode.service.ts
+++ src/providers/appMode.service.ts
@@ -3,12 +3,13 @@ import { StorageService } from './storage.service';
 
 @Injectable()
 export class AppModeService {
- 
-    HOME_MODE_KEY = "isHomeMode";
+
+    HOME_MODE_KEY = "homeMode";
     ADV_MODE_KEY = "isAdvMode";
-    
+    ONBOARDING_KEY = "shownOnboarding";
 
     constructor(private storage: StorageService) { }
+    
     getAdvMode() {
         let advMode: boolean = this.storage.get(this.ADV_MODE_KEY)
         return advMode;
@@ -16,5 +17,22 @@ export class AppModeService {
 
     setAdvMode(advMode) {
         this.storage.set(this.ADV_MODE_KEY, advMode);
+    }
+
+    getHomeMode() {
+        return this.storage.get(this.HOME_MODE_KEY);
+    }
+
+    setHomeMode() {
+        this.storage.set(this.HOME_MODE_KEY, true);
+    }
+
+    getOnboardingVisible() {
+        let isSwown: boolean = this.storage.get(this.ONBOARDING_KEY);
+        return isSwown;
+    }
+
+    setOnboardingVisible() {
+        this.storage.set(this.ONBOARDING_KEY, true);
     }
 }
--- src/providers/profile.service.ts
+++ src/providers/profile.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from "./api.service";
 import { Observable } from "rxjs";
-import { StorageService } from './storage.service';
 import { User } from '../models/user';
 
 @Injectable()
 export class ProfileService {
     user: User = new User();
 
-    constructor(private api: ApiService,
-        private storage: StorageService) { }
+    constructor(private api: ApiService) { }
 
     get() {
         return this.api.get('profile');
@@ -34,8 +32,4 @@ export class ProfileService {
         return this.api.get('profile?with=accounts');
     }
 
-    isOnboardingShown() {
-        let isSwown: boolean = this.storage.get('shownOnboarding');
-        return isSwown;
-    }
 }
