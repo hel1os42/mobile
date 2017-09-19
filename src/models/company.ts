db1b251d1a65b985faafe95540ccc83862aacf24
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-09-19 12:19:48 +0300

    added company model

---
+++ src/models/company.ts
@@ -0,0 +1,20 @@
+export class Company {
+    id: string;
+    name: string;
+    description: string;
+    about;
+    testimonials_count: number;
+    testimonials;
+    categories_count: number;
+    categories;
+    offers_count: number;
+    offers;
+    stars: number;
+    is_featured: boolean;
+    is_starred: boolean;
+    image_url;
+    address: string;
+    latitude: number;
+    longitude: number;
+    radius: number;
+}
