a30bdf32e98f8f6d1ba651eddf155a26ec0c1276
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-09-21 15:49:05 +0300

    added place fedback page bindings

--- src/pages/place-feedback/place-feedback.ts
+++ src/pages/place-feedback/place-feedback.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { OfferService } from '../../providers/offer.service';
+import { Company } from '../../models/company';
 
 @Component({
-  selector: 'page-place-feedback',
-  templateUrl: 'place-feedback.html'
+    selector: 'page-place-feedback',
+    templateUrl: 'place-feedback.html'
 })
 export class PlaceFeedbackPage {
 
-  constructor(private nav: NavController) {
+    testimonial = {};
 
-  }
+    constructor(private nav: NavController,
+        private navParams: NavParams,
+        private offers: OfferService) {
 
+    }
+
+    ionViewDidLoad() {
+        this.testimonial = this.navParams.get('testimonial')
+    }
+
+    back() {
+        this.nav.pop();
+    }
 }
