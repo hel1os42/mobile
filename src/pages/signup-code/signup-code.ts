7fcb444847512c688f9f1bf2489ed7edd815d2f9
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-09-24 21:24:27 +0300

    fixed tabs page root

--- src/pages/signup-code/signup-code.ts
+++ src/pages/signup-code/signup-code.ts
@@ -3,35 +3,38 @@ import { NavController, NavParams } from 'ionic-angular';
 import { AuthService } from "../../providers/auth.service";
 import { CreateUserProfilePage } from "../create-user-profile/create-user-profile";
 import { Register } from '../../models/register';
+import { TabsPage } from '../tabs/tabs';
+import { AppModeService } from '../../providers/appMode.service';
 
 @Component({
-  selector: 'page-signup-code',
-  templateUrl: 'signup-code.html'
+    selector: 'page-signup-code',
+    templateUrl: 'signup-code.html'
 })
 export class SignUpCodePage {
-  register: Register;
+    register: Register;
 
-  constructor(
-      private nav: NavController,
-      private auth: AuthService,
-      private navParams: NavParams) {
+    constructor(
+        private nav: NavController,
+        private auth: AuthService,
+        private appMode: AppModeService,
+        private navParams: NavParams) {
 
-      this.register = this.navParams.get('register');
-      this.register.code = this.register.phone.slice(-6);
-  }
+        this.register = this.navParams.get('register');
+        this.register.code = this.register.phone.slice(-6);
+    }
 
-  signUp() {
-    this.auth.register(this.register)
-      .subscribe(resp => {
-        this.auth
-          .login({
-            phone: this.register.phone,
-            code: this.register.code
-          })
-          .subscribe(resp => {
-            this.nav.setRoot(CreateUserProfilePage);
-          })
-      })
-  }
-
+    signUp() {
+        this.auth.register(this.register)
+            .subscribe(resp => {
+                this.auth
+                    .login({
+                        phone: this.register.phone,
+                        code: this.register.code
+                    })
+                    .subscribe(resp => {
+                        this.appMode.setHomeMode(false);
+                        this.nav.setRoot(TabsPage)
+                    })
+              })
+    }
 }
