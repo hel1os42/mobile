aa782fb55028995518cba18def9114eb381517dc
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-09-27 17:18:47 +0300

    added sub categories filter binding

--- src/pages/places/places.popover.ts
+++ src/pages/places/places.popover.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { ViewController, App } from 'ionic-angular';
+import { ViewController, App, NavParams } from 'ionic-angular';
+import { SubCategory } from '../../models/offerSubCategory';
+import { OfferService } from '../../providers/offer.service';
 
 @Component({
     selector: 'places-popover-component',
@@ -7,9 +9,20 @@ import { ViewController, App } from 'ionic-angular';
 })
 
 export class PlacesPopover {
+
+    categories: SubCategory[];
+    categoryId: string;
+
     constructor(
         private viewCtrl: ViewController,
-        private app: App) { }
+        private app: App,
+        private offer: OfferService,
+        private navParams: NavParams) { 
+        
+        this.categoryId = this.navParams.get('categoryId');
+        this.offer.getSubCategories(this.categoryId)
+            .subscribe(categories => this.categories = categories.children);
+        }
 
     close() {
         this.viewCtrl.dismiss();        
