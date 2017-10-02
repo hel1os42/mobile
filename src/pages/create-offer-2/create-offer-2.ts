09776b7f922bab7c47f98901ea9b80dbf3fc877c
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-10-02 17:48:09 +0300

    refactored date binding

--- src/pages/create-offer-2/create-offer-2.ts
+++ src/pages/create-offer-2/create-offer-2.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { OfferCreate } from '../../models/offerCreate';
+import { CreateOffer3Page } from '../create-offer-3/create-offer-3';
 
 @Component({
-  selector: 'page-create-offer-2',
-  templateUrl: 'create-offer-2.html'
+    selector: 'page-create-offer-2',
+    templateUrl: 'create-offer-2.html'
 })
 export class CreateOffer2Page {
 
-  constructor(private nav: NavController) {
+    offer = new OfferCreate;
+    isDetailedSettingsVisible: boolean = false;
 
-  }
+    constructor(private nav: NavController,
+                private navParams: NavParams) {
+
+        this.offer = this.navParams.get('offer');
+
+    }
+
+    toggleVisible() {
+        this.isDetailedSettingsVisible = !this.isDetailedSettingsVisible;
+
+    }
+
+    openCreateOffer3Page() {
+        this.nav.push(CreateOffer3Page, { offer: this.offer });
+        debugger;
+    }
 
 }
