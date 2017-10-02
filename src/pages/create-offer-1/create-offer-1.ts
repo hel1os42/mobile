840d36d4cfbdc24ace499ecd9b2dc9bb09b2193e
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-10-02 16:38:51 +0300

    added nav from create offer 1 page

--- src/pages/create-offer-1/create-offer-1.ts
+++ src/pages/create-offer-1/create-offer-1.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { OfferCreate } from '../../models/offerCreate';
+import { CreateOffer2Page } from '../create-offer-2/create-offer-2';
 
 @Component({
-  selector: 'page-create-offer-1',
-  templateUrl: 'create-offer-1.html'
+    selector: 'page-create-offer-1',
+    templateUrl: 'create-offer-1.html'
 })
 export class CreateOffer1Page {
 
-  constructor(private nav: NavController) {
+    offer = new OfferCreate();
+
+    constructor(private nav: NavController,
+                private navParams: NavParams) {
+
+        this.offer = this.navParams.get('offer');
 
   }
+
+  openCreateOffer2Page() {
+    this.nav.push(CreateOffer2Page, { offer: this.offer });//add bindings (category & type, type)
+}
 
 }
