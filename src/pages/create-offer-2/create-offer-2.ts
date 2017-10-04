333b09edb907b91aac647c3f842fd1768c3d440b
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-10-04 17:29:42 +0300

    installed moment.js moment-timezone.js

--- src/pages/create-offer-2/create-offer-2.ts
+++ src/pages/create-offer-2/create-offer-2.ts
@@ -3,6 +3,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import { OfferCreate } from '../../models/offerCreate';
 import { CreateOffer3Page } from '../create-offer-3/create-offer-3';
 import { DatePipe } from '@angular/common';
+import * as moment from 'moment';
 
 
 @Component({
@@ -19,7 +20,7 @@ export class CreateOffer2Page {
     finishTime: string;
 
     constructor(private nav: NavController,
-                private navParams: NavParams) {
+        private navParams: NavParams) {
 
         this.offer = this.navParams.get('offer');
 
@@ -32,11 +33,18 @@ export class CreateOffer2Page {
 
     openCreateOffer3Page() {
         this.offer.start_date = this.startDate + " " + this.offer.start_time;
-        this.offer.start_date = (new Date(this.offer.start_date)).toISOString();
-        this.offer.finish_date = this.finishDate + " " + this.offer.finish_time; 
-        debugger;
+        // this.offer.start_date = (new Date(this.offer.start_date)).toISOString();
+        // let start = moment(this.offer.start_date).add(2, 's').add(100000, 'ms');
+
+        // // this.offer.start_date = moment().format("YYYY-MM-DD "); 
+        // // debugger
+        // this.offer.finish_date = this.finishDate + " " + this.offer.finish_time; 
+        this.offer.start_date = "2017-09-15 16:38:17.000000+0200";//to do
+        this.offer.finish_date = "2017-11-15 16:38:17.000000+0200";
+        this.offer.start_time = "16:38:17.000000+0200";
+        this.offer.finish_time = "16:38:17.000000+0200";
         this.nav.push(CreateOffer3Page, { offer: this.offer });
-       
+
     }
 
 }
