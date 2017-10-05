8ce475911a5adabe5b683b2176b8b9a4fc630d57
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-10-05 15:56:07 +0300

    added offer create1 page temporary bindings

--- src/pages/create-offer-1/create-offer-1.ts
+++ src/pages/create-offer-1/create-offer-1.ts
@@ -10,16 +10,24 @@ import { CreateOffer2Page } from '../create-offer-2/create-offer-2';
 export class CreateOffer1Page {
 
     offer: OfferCreate;
+    discounts: number[] = [5, 10, 15, 20, 25, 30, 35, 40];//to do
+    discount: number = 10;
+    isDiscountSelectDisable: boolean = true;
 
     constructor(private nav: NavController,
-                private navParams: NavParams) {
+        private navParams: NavParams) {
 
         this.offer = this.navParams.get('offer');
 
-  }
+    }
 
-  openCreateOffer2Page() {
-    this.nav.push(CreateOffer2Page, { offer: this.offer });//add bindings (category & type, type)
-}
+    openCreateOffer2Page() {
+        this.nav.push(CreateOffer2Page, { offer: this.offer });//add bindings (category & type, type)
+    }
+
+    toggleDiscountDisabled($event, isDiscountSelectDisable) {
+        this.isDiscountSelectDisable = isDiscountSelectDisable;
+        
+    }
 
 }
