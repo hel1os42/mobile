6be033539883aa5f60d07e151e4f52f9b2ad0d02
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-10-09 11:55:54 +0300

    refactored nav from sign up  page

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import { AuthService } from '../../providers/auth.service';
 import { SignUpCodePage } from '../signup-code/signup-code';
 
