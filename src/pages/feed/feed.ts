74546b0081c0b3335ce9e0c9292c575bce8348d6
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-10-16 12:47:39 +0300

    added feed page toggle search

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-feed',
-  templateUrl: 'feed.html'
+    selector: 'page-feed',
+    templateUrl: 'feed.html'
 })
 export class FeedPage {
 
-  constructor(private nav: NavController) {
+    isVisibleSearch = false;
 
-  }
+    constructor(private nav: NavController) {
+
+    }
+
+    toggleSearch() {
+        this.isVisibleSearch = !this.isVisibleSearch;
+    }
 
 }
