e69eed1515406633b8e0f187e5a1c2d947ddbc70
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-10-17 22:54:11 +0300

    added create offer  timeframes

--- src/pages/create-offer-2/create-offer-2.ts
+++ src/pages/create-offer-2/create-offer-2.ts
@@ -4,6 +4,8 @@ import { OfferCreate } from '../../models/offerCreate';
 import { CreateOffer3Page } from '../create-offer-3/create-offer-3';
 import { DatePipe } from '@angular/common';
 import * as moment from 'moment';
+import { DateTimeUtils } from '../../utils/date-time.utils';
+
 
 @Component({
     selector: 'page-create-offer-2',
@@ -11,16 +13,57 @@ import * as moment from 'moment';
 })
 export class CreateOffer2Page {
 
-    offer = new OfferCreate;
-    isDetailedSettingsVisible: boolean = false;
+    offer: OfferCreate;
+    isDetailedSettingsVisible = false;
     todayDate: Date;
+    timeFrames = [];
+    finishTime: string;
+    startTime: string;
+    startDate: string;
+    finishDate: string;
+  
 
     constructor(private nav: NavController,
-                private navParams: NavParams) {
+        private navParams: NavParams) {
 
         this.offer = this.navParams.get('offer');
         this.todayDate = new Date();
+        let days = DateTimeUtils.ALL_DAYS;
+            
+        for (let i = 0; i < 7; i++) {
+            this.timeFrames[i] = {
+                from: '',
+                to: '',
+                days: days[i],
+                isSelected: false
+            };
+        }
+    }
 
+    selectStartTime($event) {
+        this.timeFrames.forEach((timeFrame) => {
+            timeFrame.from = this.startTime;
+            timeFrame.isSelected = true;
+        });
+    }
+
+    selectFinishTime($event) {
+        this.timeFrames.forEach((timeFrame) => {
+            timeFrame.to = this.finishTime;
+            timeFrame.isSelected = true;
+        });
+    }
+
+    selectWorkingDays($event) {
+        this.timeFrames.forEach((timeFrame) => {
+            timeFrame.isSelected = (timeFrame.days != DateTimeUtils.ALL_DAYS[5]) && (timeFrame.days !=  DateTimeUtils.ALL_DAYS[6]);
+        });
+    }
+
+    selectWeekend($event) {
+        this.timeFrames.forEach((timeFrame) => {
+            timeFrame.isSelected = (timeFrame.days ==  DateTimeUtils.ALL_DAYS[5]) || (timeFrame.days ==  DateTimeUtils.ALL_DAYS[6]);
+        });
     }
 
     toggleVisible() {
@@ -28,13 +71,23 @@ export class CreateOffer2Page {
 
     }
 
+
     openCreateOffer3Page() {
-        this.offer.start_date = this.offer.start_date + " " + this.offer.start_time;
-        this.offer.start_date = moment(this.offer.start_date).format('YYYY-MM-DD HH:mm:ss.SSSSSSZZ');// to do (timezone from place)
-        this.offer.start_time = moment(this.offer.start_date).format('HH:mm:ss.SSSSSSZZ');
-        this.offer.finish_date = this.offer.finish_date + " " + this.offer.finish_time;
-        this.offer.finish_date = moment(this.offer.finish_date).format('YYYY-MM-DD HH:mm:ss.SSSSSSZZ');// to do (timezone from place)
-        this.offer.finish_time = moment(this.offer.finish_date).format('HH:mm:ss.SSSSSSZZ')
+        let timezone = DateTimeUtils.getTimeZoneByCoords(0, 0);
+        let dateMask = DateTimeUtils.ZERO_DATETIME_SUFFIX;
+        let timeMask = DateTimeUtils.ZERO_TIME_SUFFIX; 
+        this.offer.start_date = this.startDate + dateMask + timezone;
+        this.offer.finish_date = this.finishDate + dateMask + timezone;
+
+        let selected = this.timeFrames.filter(p => p.isSelected);
+        this.offer.timeframes = selected.map(p => {
+            return {
+                from: p.from + timeMask + timezone,
+                to: p.to + timeMask + timezone,
+                days: [p.days.slice(0, 2)]
+            }
+        })
+        debugger
         this.nav.push(CreateOffer3Page, { offer: this.offer });
 
     }
