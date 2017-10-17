48a63116f7e384c878ddb9eebd5cfa272510b4f8
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-10-17 22:52:50 +0300

    added create offer 4 page temporary mocks

--- src/pages/create-offer-4/create-offer-4.ts
+++ src/pages/create-offer-4/create-offer-4.ts
@@ -10,12 +10,16 @@ import { CreateOffer5Page } from '../create-offer-5/create-offer-5';
 export class CreateOffer4Page {
 
     offer: OfferCreate;
-    levels: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    level: number = 1;
-    maxForUser: number[] = [1, 5, 10, 20, 100, 10000000];
-    maxForUs: number = 10000000;
-    maxForUserPerDay: number[] = [1, 5, 10, 20, 100, 10000000];
-    maxForUsPerDay: number =  10000000;
+    levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    level = 1;
+    maxForUser = [1, 5, 10, 20, 100, 10000000];
+    maxForUs = 10000000;
+    maxForUserPerDay = [1, 5, 10, 20, 100, 10000000];
+    maxForUsPerDay =  10000000;
+    maxForUserPerWeek = [1, 5, 10, 20, 100, 10000000];
+    maxForUsPerWeek = 10000000;
+    maxForUserPerMonth = [1, 5, 10, 20, 100, 10000000];
+    maxForUsPerMonth = 10000000;
 
     constructor(private nav: NavController,
                 private navParams: NavParams) {
@@ -26,10 +30,12 @@ export class CreateOffer4Page {
 
     openCreateOffer5Page() {
         this.offer.user_level_min = this.level;
-        this.offer.max_count = 10;//to do
+        this.offer.max_count = 100;//to do
         this.offer.max_for_user = this.maxForUs;
         this.offer.max_for_user_per_day = this.maxForUsPerDay;
-        this.offer.max_per_day = 100;//to do
+        this.offer.max_per_day = 20;//to do
+        this.offer.max_for_user_per_week = this.maxForUsPerWeek;
+        this.offer.max_for_user_per_month = this.maxForUsPerMonth;
         this.nav.push(CreateOffer5Page, { offer: this.offer });
     }
 }
