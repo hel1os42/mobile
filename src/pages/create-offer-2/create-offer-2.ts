92babe1caee3440cdc3c26a9d80156eee618192e
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-10-30 21:01:28 +0200

    added editing offers timeframes

--- src/pages/create-offer-2/create-offer-2.ts
+++ src/pages/create-offer-2/create-offer-2.ts
@@ -18,15 +18,16 @@ import { OfferDate } from '../../models/OfferDate';
 export class CreateOffer2Page {
 
     offer: Offer;
-    isDetailedSettingsVisible: boolean;
+    isDetailedSettingsVisible = false;
     todayDate: Date;
     timeFrames = [];
     finishTime: string;
     startTime: string;
     startDate: string;
     finishDate: string;
-    picture_url: string;
-
+    picture_url: string
+    isWorkingDays = false;
+    isWeekend = false;
 
     constructor(private nav: NavController,
         private navParams: NavParams,
@@ -34,7 +35,6 @@ export class CreateOffer2Page {
 
         this.offer = this.navParams.get('offer');
         this.picture_url = this.navParams.get('picture');
-        this.isDetailedSettingsVisible = this.offer.id ? true : false;
         this.todayDate = new Date();
         let days = DateTimeUtils.ALL_DAYS;
 
@@ -50,64 +50,102 @@ export class CreateOffer2Page {
             this.startDate = this.offer.start_date.date.slice(0, 10);
             this.finishDate = this.offer.finish_date.date.slice(0, 10);
             // let timeframesData = [
-            //     {from: "10:00:00.000000+0300", to: "23:00:00.000000+0300", days: ["mo"]},
-            //     ];
-            if (this.offer.timeframes.length) {
-                let timeFrames = _.flatMap(this.offer.timeframes, function (obj) {
-                    return _.map(obj.days, function (day) {
-                        return {
-                            from: obj.from,
-                            to: obj.to,
-                            days: day
-                        };
-                    });
-                });
-
-                for (let i = 0; i < this.timeFrames.length; i ++) {
-                    for (let j = 0; j < timeFrames.length; j ++) {
-                        let tTF = this.timeFrames[i];
-                        let tF = timeFrames[j];
-                        tTF.isSelected = tTF.days.slice(0, 2) == tF.days;
-                        tTF.from = (tTF.days.slice(0, 2) == tF.days) ? tF.from.slice(0, 5) : '';
-                        tTF.to = (tTF.days.slice(0, 2) == tF.days) ? tF.to.slice(0, 5) : '';
-                        if (tTF.isSelected) {
-                            break;
-                        }
-                    }
-                }
+                //{ from: "10:00:00.000000+0300", to: "23:00:00.000000+0300", days: ["tu", 'we', 'th', 'fr', "sa", "su"] },
+            // ];
+            if (this.offer.timeframes) {
+                let grouped = DateTimeUtils.groupTimeframes(this.offer.timeframes, this.timeFrames);
+                this.startTime = grouped.startTime;
+                this.finishTime = grouped.finishTime;
+                this.isWorkingDays = grouped.isWorkingDays;
+                this.isWeekend = grouped.isWeekend;
+                this.timeFrames = grouped.simpleTimeFrames;
+                this.isDetailedSettingsVisible = grouped.isDetailedSettingsVisible;
             }
+
         }
     }
 
     selectStartTime($event) {
         this.timeFrames.forEach((timeFrame) => {
-            timeFrame.from = this.startTime;
-            timeFrame.isSelected = true;
+            if (this.isWeekend || this.isWorkingDays) {
+                timeFrame.from = timeFrame.isSelected ? this.startTime : '';
+            }
+            else {
+                timeFrame.from = this.startTime;
+                timeFrame.isSelected = true;
+            }
         });
     }
 
     selectFinishTime($event) {
         this.timeFrames.forEach((timeFrame) => {
-            timeFrame.to = this.finishTime;
-            timeFrame.isSelected = true;
+            if (this.isWeekend || this.isWorkingDays) {
+                timeFrame.to = timeFrame.isSelected ? this.finishTime : '';
+            }
+            else {
+                timeFrame.to = this.finishTime;
+                timeFrame.isSelected = true;
+            }
         });
     }
 
     selectWorkingDays($event) {
         this.timeFrames.forEach((timeFrame) => {
             timeFrame.isSelected = (timeFrame.days != DateTimeUtils.SATURDAY) && (timeFrame.days != DateTimeUtils.SUNDAY);
+            timeFrame.from = timeFrame.isSelected ? this.startTime : '';
+            timeFrame.to = timeFrame.isSelected ? this.finishTime : '';
+            this.isWorkingDays = true;
+            this.isWeekend = false;
         });
     }
 
     selectWeekend($event) {
         this.timeFrames.forEach((timeFrame) => {
             timeFrame.isSelected = (timeFrame.days == DateTimeUtils.SATURDAY) || (timeFrame.days == DateTimeUtils.SUNDAY);
+            timeFrame.from = timeFrame.isSelected ? this.startTime : '';
+            timeFrame.to = timeFrame.isSelected ? this.finishTime : '';
+            this.isWeekend = true;
+            this.isWorkingDays = false;
         });
     }
 
     toggleVisible() {
         this.isDetailedSettingsVisible = !this.isDetailedSettingsVisible;
+    }
 
+    removeTime(event, isSelected) {
+        if (isSelected) {
+            switch (event) {
+                case 'start':
+                    this.startTime = DateTimeUtils.getTime(this.timeFrames).startTime;
+                    break;
+                case 'finish':
+                    this.finishTime = DateTimeUtils.getTime(this.timeFrames).finishTime;
+                    break;
+            }
+        }
+    }
+
+    checkDays(isSelected) {
+        let day = DateTimeUtils;
+        let daysFrame = this.timeFrames.filter(i => i.isSelected).map(j => j.days.slice(0, 2));
+        if (daysFrame.length == 2 || daysFrame.length == 5) {
+            if (daysFrame.length == 2) {
+                this.isWeekend = DateTimeUtils.find(daysFrame, [day.SATURDAY, day.SUNDAY]);
+                this.isWorkingDays = false;
+            }
+            if (daysFrame.length == 5) {
+                this.isWorkingDays = DateTimeUtils.find(daysFrame, [day.MONDAY, day.TUESDAY, day.WEDNESDAY, day.THURSDAY, day.FRIDAY]);
+                this.isWeekend = false;
+            }
+        }
+        else {
+            this.isWeekend = false;
+            this.isWorkingDays = false;
+        }
+
+        this.startTime = DateTimeUtils.getTime(this.timeFrames).startTime;
+        this.finishTime = DateTimeUtils.getTime(this.timeFrames).finishTime;
     }
 
     openCreateOffer3Page() {
@@ -126,7 +164,7 @@ export class CreateOffer2Page {
                     this.offer.start_date = this.startDate + dateMask + timezoneStr;
                     this.offer.finish_date = this.finishDate + dateMask + timezoneStr;
                 }
-                
+
                 let selected = this.timeFrames.filter(p => p.isSelected);
                 this.offer.timeframes = selected.map(p => {
                     return {
@@ -138,4 +176,5 @@ export class CreateOffer2Page {
                 this.nav.push(CreateOffer3Page, { offer: this.offer, picture: this.picture_url });
             });
     }
+
 }
