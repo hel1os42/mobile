00a32b744cf0c3bfffa58fb9af535436f3ceae98
Oleksandr Khoronko <o.khoronko@toavalon.com>
2017-10-31 14:41:18 +0200

    Add chart page Statistic1

--- src/pages/statistic1/statistic1.ts
+++ src/pages/statistic1/statistic1.ts
@@ -1,13 +1,86 @@
-import { Component } from '@angular/core';
+import { Chart } from 'chart.js';
+import { Component, ViewChild } from '@angular/core';
+var array_gradient = [];
 
 @Component({
   selector: 'page-statistic1',
   templateUrl: 'statistic1.html'
 })
+
 export class Statistic1Page {
 
-  constructor() {
+    @ViewChild('barCanvas') barCanvas;
 
-  }
+    barChart: any;
+
+    constructor() {
+    }
+
+    ionViewDidLoad() {
+        Chart.defaults.global.defaultFontColor = '#fff';
+        this.barChart = new Chart(this.barCanvas.nativeElement, {
+
+            type: 'bar',
+            data: {
+                labels: [['PERIOD 1', '01/07/2017'], ['PERIOD 2', '01/07/2017']],
+                datasets: [{
+                    data: [435, 335],
+                    borderColor: '#FF640C',
+                    backgroundColor: '#FF640C'
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{ // titles axes
+                        ticks: {
+                            stepSize: 100,
+                            beginAtZero:true,
+                            padding: 20,
+                        },
+                        gridLines: {
+                            color: '#606A78',
+                            lineWidth: 1,
+                            drawBorder: false
+                        }
+                    }],
+                    xAxes: [{ // titles axes
+                        ticks: {
+                            padding: 20
+                        },
+                        gridLines: {
+                            display: false
+                        }
+                    }],
+                },
+                legend:{
+                    display:false,
+                },
+                tooltips: {
+                    callbacks: {
+                        title: function(tooltipItem, data) {
+                            return data['labels'][tooltipItem[0]['index']];
+                        },
+                        label: function(tooltipItem, data) {
+                            return data['datasets'][0]['data'][tooltipItem['index']];
+                        },
+                        afterLabel: function(tooltipItem, data) {
+                            var dataset = data['datasets'][0];
+                            var percent = Math.round((dataset['data'][tooltipItem['index']] / dataset["_meta"][0]['total']) * 100)
+                            return '(' + percent + '%)';
+                        }
+                    },
+                    mode: 'point',
+                    backgroundColor: '#FFF',
+                    titleFontSize: 16,
+                    titleFontColor: '#0066ff',
+                    bodyFontColor: '#000',
+                    bodyFontSize: 14,
+                    displayColors: false
+                }
+            }
+
+        });
+
+    }
 
 }
