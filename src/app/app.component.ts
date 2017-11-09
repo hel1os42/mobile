8882d19076cfefcb6b6f21c5791c2ee1f1743773
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-11-09 20:45:47 +0200

    removed redundant imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
-import { Platform, App } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { TabsPage } from "../pages/tabs/tabs";
-import { AuthService } from "../providers/auth.service";
-import { OnBoardingPage } from "../pages/onboarding/onboarding";
-import { CreateAdvUserProfilePage } from "../pages/create-advUser-profile/create-advUser-profile";
-import { CreateOfferPage } from "../pages/create-offer/create-offer";
-import { AdvUserOffersPage } from "../pages/adv-user-offers/adv-user-offers";
-import { LoginPage } from "../pages/login/login";
-import { CreateUserProfilePage } from '../pages/create-user-profile/create-user-profile';
-import { AdvTabsPage } from '../pages/adv-tabs/adv-tabs';
-import { StatisticPage } from '../pages/statistic/statistic';
+import { StatusBar } from '@ionic-native/status-bar';
+import { App, Platform } from 'ionic-angular';
+
+import { LoginPage } from '../pages/login/login';
+import { OnBoardingPage } from '../pages/onboarding/onboarding';
+import { TabsPage } from '../pages/tabs/tabs';
+import { AuthService } from '../providers/auth.service';
 
 @Component({
     templateUrl: 'app.html'
