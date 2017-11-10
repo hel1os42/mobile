be53b52db9452325efec1e568d17189c237a92e6
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-11-10 12:43:06 +0200

    refactored  root page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { App, Platform } from 'ionic-angular';
-
 import { LoginPage } from '../pages/login/login';
 import { OnBoardingPage } from '../pages/onboarding/onboarding';
 import { TabsPage } from '../pages/tabs/tabs';
 import { AuthService } from '../providers/auth.service';
+import { ProfileService } from "../providers/profile.service";
+import { CreateUserProfilePage } from '../pages/create-user-profile/create-user-profile';
 
 @Component({
     templateUrl: 'app.html'
@@ -14,9 +15,12 @@ import { AuthService } from '../providers/auth.service';
 export class MyApp {
     rootPage: any;
 
-    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+    constructor(platform: Platform,
+        statusBar: StatusBar,
+        splashScreen: SplashScreen,
         private auth: AuthService,
-        private app: App) {
+        private app: App,
+        private profile: ProfileService) {
 
         platform.ready().then((resp) => {
             // Okay, so the platform is ready and our plugins are available.
@@ -24,7 +28,12 @@ export class MyApp {
             statusBar.styleDefault();
             splashScreen.hide();
 
-            this.rootPage = !this.auth.isLoggedIn() ? OnBoardingPage : TabsPage;
+            let page;
+            this.profile.get()
+                .subscribe(resp => {
+                    page = (resp.name == '' && !resp.email) ? CreateUserProfilePage : TabsPage;
+                    this.rootPage = !this.auth.isLoggedIn() ? OnBoardingPage : page;
+                });
 
             //this.rootPage = StatisticPage;
         });
