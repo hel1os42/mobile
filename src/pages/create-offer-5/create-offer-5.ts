8fe9dba988518df1b1db8ea70a7bea4808bce616
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-11-14 14:06:07 +0200

    refactored offer image upload

--- src/pages/create-offer-5/create-offer-5.ts
+++ src/pages/create-offer-5/create-offer-5.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ProfileService } from '../../providers/profile.service';
 import { PlaceService } from '../../providers/place.service';
 import { ApiService } from '../../providers/api.service';
@@ -17,12 +17,14 @@ export class CreateOffer5Page {
     reward = '1';
     reserved = '10';
     picture_url: string;
+    timer;
 
     constructor(private nav: NavController,
         private navParams: NavParams,
         private place: PlaceService,
         private profile: ProfileService,
-        private api: ApiService) {
+        private api: ApiService,
+        private loading: LoadingController) {
 
         this.offer = this.navParams.get('offer');
         this.picture_url = this.navParams.get('picture');
@@ -53,10 +55,21 @@ export class CreateOffer5Page {
                         let offer_id = location.slice(- location.lastIndexOf('/') + 2);
                         console.log("locaction: " + location);
                         console.log("parsing: " + offer_id);
-
                         if (this.picture_url) {
-                            this.api.uploadImage(this.picture_url, `offers/${offer_id}/picture`)
-                                .then(resut => this.nav.setRoot(AdvTabsPage));
+
+                            this.timer = setInterval(() => {
+                                let loading = this.loading.create({ content: 'Creating your offer...' });
+                                loading.present();
+                                this.place.getOffer(offer_id, false)
+                                    .subscribe(offer => {
+                                        if (offer) {
+                                            this.stopTimer();
+                                            loading.dismiss();
+                                            this.api.uploadImage(this.picture_url, `offers/${offer_id}/picture`)
+                                                .then(resut => this.nav.setRoot(AdvTabsPage));
+                                        }
+                                    });
+                            }, 2000)
                         }
                         else {
                             this.nav.popToRoot();
@@ -66,6 +79,13 @@ export class CreateOffer5Page {
                         this.nav.popToRoot();
                     }
                 })
+        }
+    }
+
+    stopTimer() {
+        if (this.timer) {
+            clearInterval(this.timer);
+            this.timer = undefined;
         }
     }
 
