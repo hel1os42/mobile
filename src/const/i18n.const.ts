bd9de2645d9487d64b44819b2fdf69f3b75922ff
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-11-22 10:37:39 +0200

    basic translation module + logic

---
+++ src/const/i18n.const.ts
@@ -0,0 +1,13 @@
+export const AVAILABLE_LANGUAGES = [{
+	code: 'en',
+	name: 'English'
+}, {
+	code: 'ru',
+	name: 'Russian'
+}];
+
+export const DEFAULT_LANG_CODE = 'en';
+
+export const SYS_OPTIONS = {
+	LANG_CODE: DEFAULT_LANG_CODE
+};
