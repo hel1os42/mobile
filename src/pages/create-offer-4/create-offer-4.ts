e345eec5a2099f7d492691d3ee7bcbaaa9d50ccc
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-11-28 22:16:57 +0200

    -refactored create offer 4 page -added basic validation on create offer 4 page

--- src/pages/create-offer-4/create-offer-4.ts
+++ src/pages/create-offer-4/create-offer-4.ts
@@ -1,7 +1,8 @@
-import { ToastService } from '../../providers/toast.service';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { NavController, NavParams } from 'ionic-angular';
-import { OfferCreate } from '../../models/offerCreate';
+import { Offer } from '../../models/offer';
+import { ToastService } from '../../providers/toast.service';
 import { CreateOffer5Page } from '../create-offer-5/create-offer-5';
 
 @Component({
@@ -10,50 +11,102 @@ import { CreateOffer5Page } from '../create-offer-5/create-offer-5';
 })
 export class CreateOffer4Page {
 
-    offer: OfferCreate;
+    offer: Offer;
     levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     level = 1;
-    maxForUser;
-    maxForUserPerDay;
-    maxForUserPerWeek;
-    maxForUserPerMonth;
     picture_url: string;
+    maxData: FormGroup;
 
     constructor(private nav: NavController,
-                private navParams: NavParams,
-                private toast: ToastService) {
+        private navParams: NavParams,
+        private toast: ToastService,
+        private builder: FormBuilder) {
 
         this.offer = this.navParams.get('offer');
         this.picture_url = this.navParams.get('picture');
 
+        this.maxData = this.builder.group({
+            maxCount: [this.offer.max_count],
+            dayMaxCount: [this.offer.max_per_day],
+            maxForUser: [this.offer.max_for_user],
+            maxForUserPerDay: [this.offer.max_for_user_per_day],
+            maxForUserPerWeek: [this.offer.max_for_user_per_week],
+            maxForUserPerMonth: [this.offer.max_for_user_per_month],
+        })
     }
 
-    validate() {
-        let maxForUser = parseInt(this.maxForUser);
-        let maxForUserPerDay = parseInt(this.maxForUserPerDay);
-        if (maxForUserPerDay > 0 && maxForUser >= 0 && maxForUserPerDay > maxForUser) {
-            this.toast.show('Max for user valud should be less than max overall');
+    validateMaxForUser() {
+        let maxForUser = parseInt(this.maxData.value.maxForUser);
+        let maxForUserPerDay = parseInt(this.maxData.value.maxForUserPerDay);
+        let maxForUserPerWeek = parseInt(this.maxData.value.maxForUserPerWeek);
+        let maxForUserPerMonth = parseInt(this.maxData.value.maxForUserPerMonth);
+
+        if (maxForUser >= 0 && (maxForUserPerDay > 0 && maxForUserPerDay > maxForUser) ||
+            (maxForUserPerWeek > 0 && maxForUserPerWeek > maxForUser) ||
+            (maxForUserPerMonth > 0 && maxForUserPerMonth > maxForUser)) {
+            this.toast.show('Max for user valid should be less than max for user overall');
             return false;
         };
         return true;
     }
 
+    validateMax() {
+        let maxCount = parseInt(this.maxData.value.maxCount);
+        let dayMaxCount = parseInt(this.maxData.value.dayMaxCount);
+
+        if (maxCount >= 0 && dayMaxCount > 0 && dayMaxCount > maxCount) {
+            this.toast.show('Day max valid should be less than max overall');
+            return false;
+        };
+        return true;
+    }
+
+    validateBetween() {
+        let maxCount = parseInt(this.maxData.value.maxCount);
+        let dayMaxCount = parseInt(this.maxData.value.dayMaxCount);
+        let maxForUser = parseInt(this.maxData.value.maxForUser);
+        let maxForUserPerDay = parseInt(this.maxData.value.maxForUserPerDay);
+
+        if ((maxCount >= 0 && maxForUser > 0 && maxForUser > maxCount) ||
+            (dayMaxCount >= 0 && maxForUserPerDay > 0 && maxForUserPerDay > dayMaxCount)) {
+            this.toast.show('Max for user valid should be less than max overall');
+            return false;
+        };
+        return true;
+    }
+
+    updateList(ev) {
+        ev.target.value = ev.target.value.replace(/D/g, '');
+    }
+
+    limitStr(key) {
+        this.maxData.valueChanges.subscribe(data => {
+            if (data[key].length > 18) {
+                this.maxData.controls[key].setValue(data[key].slice(0, 18));
+            }
+        })
+    }
+
     openCreateOffer5Page() {
-        if (this.validate()) {
+        if (this.validateMaxForUser() && this.validateMax() && this.validateBetween()) {
             this.offer.user_level_min = this.level;
-            this.offer.max_count = 100;//to do
-            this.offer.max_per_day = 20;//to do
-            if(this.maxForUser) {
-                this.offer.max_for_user = parseInt(this.maxForUser);
+            if (this.maxData.value.maxCount) {
+                this.offer.max_count = parseInt(this.maxData.value.maxCount);
             }
-            if(this.maxForUserPerDay) {
-                this.offer.max_for_user_per_day = parseInt(this.maxForUserPerDay);
+            if (this.maxData.value.dayMaxCount) {
+                this.offer.max_per_day = parseInt(this.maxData.value.dayMaxCount);
             }
-            if(this.maxForUserPerWeek) {
-                this.offer.max_for_user_per_week = parseInt(this.maxForUserPerWeek);
+            if (this.maxData.value.maxForUser) {
+                this.offer.max_for_user = parseInt(this.maxData.value.maxForUser);
             }
-            if(this.maxForUserPerMonth) {
-                this.offer.max_for_user_per_month = parseInt(this.maxForUserPerMonth);
+            if (this.maxData.value.maxForUserPerDay) {
+                this.offer.max_for_user_per_day = parseInt(this.maxData.value.maxForUserPerDay);
+            }
+            if (this.maxData.value.maxForUserPerWeek) {
+                this.offer.max_for_user_per_week = parseInt(this.maxData.value.maxForUserPerWeek);
+            }
+            if (this.maxData.value.maxForUserPerMonth) {
+                this.offer.max_for_user_per_month = parseInt(this.maxData.value.maxForUserPerMonth);
             }
             this.nav.push(CreateOffer5Page, { offer: this.offer, picture: this.picture_url });
         }
