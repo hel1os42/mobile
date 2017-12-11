0e9f1079861f7b263f423095963ccc2f909b8cd2
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-12-11 16:19:35 +0200

    dev mode  emitter

--- src/providers/appMode.service.ts
+++ src/providers/appMode.service.ts
@@ -1,3 +1,4 @@
+import { TokenService } from './token.service';
 import { Injectable, EventEmitter } from '@angular/core';
 import { StorageService } from './storage.service';
 
@@ -7,11 +8,15 @@ export class AppModeService {
     HOME_MODE_KEY = 'homeMode';
     ADV_MODE_KEY = 'isAdvMode';
     ONBOARDING_KEY = 'shownOnboarding';
-    DEV_MODE_KEY = 'isDevMOde'
+    DEV_MODE_KEY = 'isDevMode';
 
     onHomeChange = new EventEmitter<boolean>();
+    onDevMode = new EventEmitter<boolean>();
 
-    constructor(private storage: StorageService) { }
+    constructor(private storage: StorageService,
+        private token: TokenService) {
+        this.token.onRemove.subscribe(() => this.removeDevMode());
+    }
 
     getAdvMode() {
         return !!this.storage.get(this.ADV_MODE_KEY)
@@ -31,10 +36,12 @@ export class AppModeService {
 
     setDevMode(isDevMode: boolean) {
         this.storage.set(this.DEV_MODE_KEY, isDevMode);
+        this.onDevMode.emit(isDevMode);
     }
 
     removeDevMode() {
         this.storage.remove(this.DEV_MODE_KEY);
+        this.onDevMode.emit(false);
     }
 
     setHomeMode(showPlaces: boolean) {
