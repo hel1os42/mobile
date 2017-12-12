dc3a00dfeab64d61adc2dd8d0d25b0debddb8d83
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-12-12 18:41:22 +0200

    added transfer page

---
+++ src/pages/transfer/transfer.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'page-transfer',
+    templateUrl: 'transfer.html'
+})
+export class TransferPage {
+
+    constructor() {
+
+    }
+}
