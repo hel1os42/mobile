6faba345117ee7842be5c93d6be1eac688ae87b0
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-12-13 13:21:23 +0200

    added transfer data validation

--- src/pages/transfer/transfer.ts
+++ src/pages/transfer/transfer.ts
@@ -1,8 +1,11 @@
-import { NavParams } from 'ionic-angular';
+import { NamberValidator } from '../../validators/number.validator';
+import { NavParams, NavController } from 'ionic-angular';
 import { ProfileService } from '../../providers/profile.service';
 import { TransactionCreate } from '../../models/transactionCreate';
 import { Component } from '@angular/core';
 import { Account } from '../../models/account';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastService } from '../../providers/toast.service';
 
 @Component({
     selector: 'page-transfer',
@@ -12,11 +15,65 @@ export class TransferPage {
 
     NAU: Account;
     transferData: TransactionCreate;
+    sourceAddress: string;
+    destinationAddress: string;
+    amount: number;
+    balance: number;
+    formData: FormGroup;
 
     constructor(
         private profile: ProfileService,
-        private navParams: NavParams) {
+        private navParams: NavParams,
+        private builder: FormBuilder,
+        private toast: ToastService,
+        private nav: NavController) {
 
         this.NAU = this.navParams.get('NAU');
+        this.sourceAddress = this.NAU.address;
+        this.balance = this.NAU.balance;
+
+        this.formData = this.builder.group({
+            sourceAddress: new FormControl(this.sourceAddress, Validators.compose([
+                Validators.required,
+                Validators.minLength(20),
+                Validators.pattern('^[a-zA-Z0-9]*$'),
+            ])),
+            destinationAddress: new FormControl('', Validators.compose([
+                Validators.required,
+                Validators.minLength(20),
+                Validators.pattern('^[a-zA-Z0-9]*$'),
+            ])),
+            amount: new FormControl('1', Validators.compose([
+                NamberValidator.min(1),
+                Validators.required,
+            ])),
+        });
+    }
+
+    validateMax() {
+        let amount = parseInt(this.formData.value.amount);
+        if (amount > this.balance) {
+            this.toast.show('Amount should be no more then balance');
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    transfer() {
+        if (this.validateMax) {
+            let data = this.formData.value;
+            this.transferData = {
+                source: data.sourceAddress,
+                destination: data.destinationAddress,
+                amount: parseInt(data.amount)
+            };
+        this.profile.postTransaction(this.transferData)
+            .subscribe(() => {
+                this.profile.refreshAccounts();
+                this.nav.pop();
+            })
+        }
     }
 }
