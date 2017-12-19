71e0bf66b7afe1289bbd8565ad22cfd562da4abf
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-12-19 23:16:13 +0200

    place types mock

--- src/mocks/mockPlaceTypes.ts
+++ src/mocks/mockPlaceTypes.ts
@@ -1,8 +1,32 @@
 export class MockPlaceTypes {
-    public static retailTypes : [
-        'Restaurants',
-        'Cafes',
-        'Bars',
-        'Informal & Takeaway'
+
+    public static RetailTypes = [
+        { name: 'Restaurants' },
+        { name: 'Cafes' },
+        { name: 'Bars' },
+        { name: 'Informal & Takeaway' }
+    ];
+
+    public static Features = [
+        { name: 'Alcohol' },
+        { name: 'Big Screen' },
+        { name: 'Cards Accepted' },
+        { name: 'Kids Welcome' },
+        { name: 'Kids Room' },
+        { name: 'Live Music' },
+        { name: 'Outdoor Available' },
+        { name: 'Outdoor Heating' },
+        { name: 'Parking' },
+        { name: 'Pets Welcome' },
+        { name: 'Price Level: Low' },
+        { name: 'Price Level: Medium' },
+        { name: 'Price Level: High' },
+        { name: 'Reservation Only' },
+        { name: 'Smoking Indoor' },
+        { name: 'Shisha' },
+        { name: 'Takeaway' },
+        { name: 'Wheelchair Accessible' },
+        { name: 'Wi-Fi' },
+        { name: 'Delivery' }
     ]
 }
