3f62ec558597a7dc7f270f7aa8dfce1d638ae2e1
Serhii Poraiko <serhii.poraiko@toavalon.com>
2017-12-29 01:37:01 +0200

    refactored create-offer2.ts

--- src/pages/create-offer-2/create-offer-2.ts
+++ src/pages/create-offer-2/create-offer-2.ts
@@ -22,43 +22,51 @@ export class CreateOffer2Page {
     picture_url: string;
     isWorkingDays = false;
     isWeekend = false;
+    timezoneStr: string;
 
     constructor(private nav: NavController,
         private navParams: NavParams,
-        private timezone: TimezoneService) {
+        private timezoneService: TimezoneService) {
 
         this.offer = this.navParams.get('offer');
         this.picture_url = this.navParams.get('picture');
         this.todayDate = new Date();
         let days = DateTimeUtils.ALL_DAYS;
-        this.startDate = (this.offer.id && this.offer.start_date.date) ? 
-        this.offer.start_date.date.slice(0, 10) : this.todayDate.toISOString().slice(0, 10);
-        this.finishDate = (this.offer.id && this.offer.finish_date.date) ? 
-        this.offer.finish_date.date.slice(0, 10) : this.todayDate.toISOString().slice(0, 10);
 
-        for (let i = 0; i < 7; i++) {
-            this.timeFrames[i] = {
-                from: '',
-                to: '',
-                days: days[i],
-                isSelected: false
-            };
-        }
-        if (this.offer.id) {
-            // let timeframesData = [
-                //{ from: "10:00:00.000000+0300", to: "23:00:00.000000+0300", days: ["tu", 'we', 'th', 'fr', "sa", "su"] },
-            // ];
-            if (this.offer.timeframes) {
-                let grouped = DateTimeUtils.groupTimeframes(this.offer.timeframes, this.timeFrames);
-                this.startTime = grouped.startTime;
-                this.finishTime = grouped.finishTime;
-                this.isWorkingDays = grouped.isWorkingDays;
-                this.isWeekend = grouped.isWeekend;
-                this.timeFrames = grouped.simpleTimeFrames;
-                this.isDetailedSettingsVisible = grouped.isDetailedSettingsVisible;
-            }
+        this.timezoneService.get(this.offer.latitude, this.offer.longitude, Math.round(this.todayDate.valueOf() / 1000))
+            .subscribe(resp => {
+                let timezone = DateTimeUtils.getTimezone(resp).timezone;
+                this.timezoneStr = DateTimeUtils.getTimezone(resp).timezoneStr;
 
-        }
+                this.startDate = (this.offer.id && this.offer.start_date.date)
+                    ? DateTimeUtils.returnDate(this.offer.start_date.date, timezone)
+                    : this.todayDate.toISOString().slice(0, 10);
+
+                this.finishDate = (this.offer.id && this.offer.finish_date.date)
+                    ? DateTimeUtils.returnDate(this.offer.finish_date.date, timezone)
+                    : this.todayDate.toISOString().slice(0, 10);
+
+                for (let i = 0; i < 7; i++) {
+                    this.timeFrames[i] = {
+                        from: '',
+                        to: '',
+                        days: days[i],
+                        isSelected: false
+                    };
+                }
+                if (this.offer.id) {
+
+                    if (this.offer.timeframes) {
+                        let grouped = DateTimeUtils.groupTimeframes(this.offer.timeframes, this.timeFrames, timezone);
+                        this.startTime = grouped.startTime;
+                        this.finishTime = grouped.finishTime;
+                        this.isWorkingDays = grouped.isWorkingDays;
+                        this.isWeekend = grouped.isWeekend;
+                        this.timeFrames = grouped.simpleTimeFrames;
+                        this.isDetailedSettingsVisible = grouped.isDetailedSettingsVisible;
+                    }
+                }
+            });
     }
 
     selectStartTime($event) {
@@ -164,7 +172,7 @@ export class CreateOffer2Page {
         // });
     }
 
-    isDisabled () {
+    isDisabled() {
         let disabled;
         let selected = this.timeFrames.filter(p => p.from != '' && p.from && p.to && p.to != '' && p.isSelected);
         disabled = (selected.length == 0 || !this.startDate || !this.finishDate) ? true : false;
@@ -172,26 +180,20 @@ export class CreateOffer2Page {
     }
 
     openCreateOffer3Page() {
-        let timezone: number;
-        let timezoneStr: string;
-        this.timezone.get(this.offer.latitude, this.offer.longitude, Math.round(this.todayDate.valueOf() / 1000))
-            .subscribe(resp => {
-                timezoneStr = DateTimeUtils.getTimezone(resp);
-                let startDateMask = DateTimeUtils.ZERO_START_DATETIME_SUFFIX;
-                let finishDateMask = DateTimeUtils.ZERO_FINISH_DATETIME_SUFFIX;
-                let timeMask = DateTimeUtils.ZERO_TIME_SUFFIX;
-                    this.offer.start_date = this.startDate + startDateMask + timezoneStr;
-                    this.offer.finish_date = this.finishDate + finishDateMask + timezoneStr;
-                let selected = this.timeFrames.filter(p => p.isSelected);
-                this.offer.timeframes = selected.map(p => {
-                    return {
-                        from: p.from + timeMask + timezoneStr,
-                        to: p.to + timeMask + timezoneStr,
-                        days: [p.days.slice(0, 2)]
-                    }
-                })
-                this.nav.push(CreateOffer3Page, { offer: this.offer, picture: this.picture_url });
-            });
+        let startDateMask = DateTimeUtils.ZERO_START_DATETIME_SUFFIX;
+        let finishDateMask = DateTimeUtils.ZERO_FINISH_DATETIME_SUFFIX;
+        let timeMask = DateTimeUtils.ZERO_TIME_SUFFIX;
+        this.offer.start_date = this.startDate + startDateMask + this.timezoneStr;
+        this.offer.finish_date = this.finishDate + finishDateMask + this.timezoneStr;
+        let selected = this.timeFrames.filter(p => p.isSelected);
+        this.offer.timeframes = selected.map(p => {
+            return {
+                from: p.from + timeMask + this.timezoneStr,
+                to: p.to + timeMask + this.timezoneStr,
+                days: [p.days.slice(0, 2)]
+            }
+        })
+        this.nav.push(CreateOffer3Page, { offer: this.offer, picture: this.picture_url });
     }
 
 }
