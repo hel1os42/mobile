d0c97b5338b30e4bbd061a2a6ec8db360f5a6c40
Serhii Poraiko <serhii.poraiko@toavalon.com>
2018-01-18 15:55:43 +0200

    added place's mock

---
+++ src/mocks/mockPlace.ts
@@ -0,0 +1,49 @@
+export class MockPlace {
+
+    public static place = {
+        "cover_url": "",
+        "picture_url": "",
+        "is_starred": false,
+        "is_featured": false,
+        "stars": 0,
+        "offers_count": 0,
+        "testimonials": "",
+        "testimonials_count": 0,
+        "id": "",
+        "user_id": "",
+        "name": "Skido place1",
+        "description": "place desrc",
+        "about": "about place",
+        "address": "place address",
+        "category": "aea58d61-5ad5-4f79-9a2c-819245f56696",
+        "retail_types": [
+            "9fe24051-dca7-49e3-b5c2-ee164738c4c5",
+            "305eab62-94ed-480c-8d88-a72a9f4d0eda"
+        ],
+        "latitude": 45.545545,
+        "longitude": 25.566656,
+        "radius": 20000,
+        "tags": [
+            "tag-bar-food",
+            "tag-fast-food",
+            "tag-pizza"
+        ],
+        "specialities": [
+            {
+                "retail_type_id": "9fe24051-dca7-49e3-b5c2-ee164738c4c5",
+                "specs": [
+                    "spec-alcohol",
+                    "spec-wi-fi",
+                    "spec-price-level-medium"
+                ]
+            },
+            {
+                "retail_type_id": "305eab62-94ed-480c-8d88-a72a9f4d0eda",
+                "specs": [
+                    "spec-big-screen",
+                    "spec-price-level-high"
+                ]
+            }
+        ]
+    }
+}
