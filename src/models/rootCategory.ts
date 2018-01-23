cf43a4eb456bfc26ab2b09a39b0bdf798a383560
Serhii Poraiko <serhii.poraiko@toavalon.com>
2018-01-23 17:04:12 +0200

    refactored root category model

--- src/models/rootCategory.ts
+++ src/models/rootCategory.ts
@@ -4,9 +4,10 @@ import { Tag } from "./tag";
 export class RootCategory {
     id: string;
     name: string;
-    parent_id?: any;
     children_count: number;
     parent?: any;
     retail_types: RetailType[];
-    tags: Tag[];
+    parent_id: string;
+    pivot;
+    tags?: Tag[];
 }
