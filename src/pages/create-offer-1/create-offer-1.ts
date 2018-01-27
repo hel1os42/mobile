bb8c6daf732f6194da29dacbfa8baec38255e6d8
Serhii Poraiko <serhii.poraiko@toavalon.com>
2018-01-28 00:07:55 +0200

    basic offer type setting logic

--- src/pages/create-offer-1/create-offer-1.ts
+++ src/pages/create-offer-1/create-offer-1.ts
@@ -5,6 +5,7 @@ import { Place } from '../../models/place';
 import { PlaceService } from '../../providers/place.service';
 import { StringValidator } from '../../validators/string.validator';
 import { CreateOffer2Page } from '../create-offer-2/create-offer-2';
+import { ToastService } from '../../providers/toast.service';
 
 @Component({
     selector: 'page-create-offer-1',
@@ -15,16 +16,23 @@ export class CreateOffer1Page {
     offer: Offer;
     discounts = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];//to do
     discount: number = 10;
-    isDiscountSelectDisable = true;
-    isGiftSelectDisable = true;
+    isDiscountHidden = true;
+    isGiftBonusHidden = true;
     company: Place;
     picture_url: string;
+    giftBonusDescr: string;
+    type: string;
 
-    constructor(private nav: NavController,
+    constructor(
+        private nav: NavController,
         private navParams: NavParams,
-        private place: PlaceService) {
+        private place: PlaceService,
+        private toast: ToastService) {
 
         this.offer = this.navParams.get('offer');
+        if (this.offer.type) {
+            this.type = this.offer.type;
+        }
         this.picture_url = this.navParams.get('picture');
         this.place.getWithCategory()
             .subscribe(company => {
@@ -35,27 +43,44 @@ export class CreateOffer1Page {
             });
     }
 
-    openCreateOffer2Page() {
-        if (!this.offer.id) {
-            this.offer.longitude = this.company.longitude;
-            this.offer.latitude = this.company.latitude;
-            this.offer.radius = this.company.radius;
-        }
-        this.nav.push(CreateOffer2Page, { offer: this.offer, picture: this.picture_url });//add bindings (category & type, type)
-    }
-
     toggleDiscountDisabled(event, isDiscountSelectDisable) {
-        this.isDiscountSelectDisable = isDiscountSelectDisable;
-        if (event == 'bonus') {
-            this.isGiftSelectDisable = false;
+        this.isDiscountHidden = isDiscountSelectDisable;
+        if (event === 'bonus' || event === 'gift') {
+            this.isGiftBonusHidden = false;
         }
         else {
-            this.isGiftSelectDisable = true;
+            this.isGiftBonusHidden = true;
         }
+        this.type = event;
     }
 
     updateAmount(event) {
         StringValidator.stringAmountLimit(event);
+    }
+
+    validate() {
+        if (!this.type) {// to do
+            this.toast.show('Please, set offer type with description!');
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    openCreateOffer2Page() {
+        if (this.validate()) {
+            this.offer.type = this.type;
+            if (this.offer.type === 'gift' || this.offer.type === 'bonus') {
+                this.offer.gift_bonus_descr = this.giftBonusDescr;
+            }
+            if (!this.offer.id) {
+                this.offer.longitude = this.company.longitude;
+                this.offer.latitude = this.company.latitude;
+                this.offer.radius = this.company.radius;
+            }
+            this.nav.push(CreateOffer2Page, { offer: this.offer, picture: this.picture_url });//add bindings (category & type, type)
+        }
     }
 
 }
