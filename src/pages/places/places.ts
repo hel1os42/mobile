efe13e312bbef4f3d4ccbeba12eb5402d1ae0e4c
Oleksandr Khoronko <o.khoronko@toavalon.com>
2018-03-12 14:28:44 +0200

    Support keyboard number from IOS. Fix shadow in items bookmarks.

--- src/pages/places/places.ts
+++ src/pages/places/places.ts
@@ -655,4 +655,9 @@ export class PlacesPage {
             this.onRefreshListSubscription.unsubscribe();
         }
     }
+
+    ionViewDidLoad() {
+        //let imgEl: HTMLElement = document.getElementsByClassName('test')[0].getElementsByClassName('scroll-content')[0];
+        //imgEl.style.marginBottom = '0'
+    }
 }
