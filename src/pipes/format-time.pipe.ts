7e9d3786d478c7d712cad9a26291633e4f7ad676
Serhii Poraiko <serhii.poraiko@toavalon.com>
2018-03-12 15:38:38 +0200

    minor refactored time pipe

--- src/pipes/format-time.pipe.ts
+++ src/pipes/format-time.pipe.ts
@@ -7,6 +7,10 @@ export class FormatTimePipe implements PipeTransform {
 
     transform(value: number): string {
         const minutes: number = Math.floor(value / 60);
-        return ('00' + minutes).slice(-2) + ':' + ('00' + Math.floor(value - minutes * 60)).slice(-2);
+        // return ('00' + minutes).slice(-2) + ':' + ('00' + Math.floor(value - minutes * 60)).slice(-2);
+        let string = value >= 10 
+        ? ('00' + Math.floor(value - minutes * 60)).slice(-2) 
+        : (Math.floor(value - minutes * 60)).toString().slice(-2);
+        return string;
     }
 }
