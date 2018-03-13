5d8c582415dee244c5a2952f056be7273a79c9cd
Serhii Poraiko <serhii.poraiko@toavalon.com>
2018-03-13 12:55:48 +0200

    removed temporary page from nav

--- src/pages/signup-code/signup-code.ts
+++ src/pages/signup-code/signup-code.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Observable } from 'rxjs';
 import { Register } from '../../models/register';
 import { AppModeService } from '../../providers/appMode.service';
 import { AuthService } from '../../providers/auth.service';
 import { StringValidator } from '../../validators/string.validator';
-import { TemporaryPage } from '../temporary/temporary';
-import { Observable } from 'rxjs';
+import { CreateUserProfilePage } from '../create-user-profile/create-user-profile';
 
 @Component({
     selector: 'page-signup-code',
@@ -82,8 +82,8 @@ export class SignUpCodePage {
                         this.cancelTimer();
                         this.isRetry = false;
                         // this.nav.setRoot(CreateUserProfilePage, { user: resp});
-                        // this.nav.setRoot(CreateUserProfilePage);temporary
-                        this.nav.setRoot(TemporaryPage);// temporary
+                        this.nav.setRoot(CreateUserProfilePage);
+                        // this.nav.setRoot(TemporaryPage);
                     })
             })
     }
