5d9e0565e97f2f8be494fe06e1693319f6664523
Serhii Poraiko <serhii.poraiko@toavalon.com>
2018-03-19 14:30:47 +0200

    refactored geocode service method

--- src/providers/geocode.service.ts
+++ src/providers/geocode.service.ts
@@ -2,35 +2,42 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import { ToastService } from './toast.service';
+import { SYS_OPTIONS } from '../const/i18n.const';
 
 
 @Injectable()
 export class GeocodeService {
 
-url = 'https://nominatim.openstreetmap.org/reverse?format=jsonv2';
+    url = 'https://nominatim.openstreetmap.org/reverse?format=jsonv2';
 
-constructor(private http: Http,
-            private toast: ToastService) {
+    constructor(private http: Http,
+        private toast: ToastService) {
 
-}
-    
-getAddress(lat: number, lng: number, isEn?: boolean) {
-        // this.url = this.url + '&accept-language=ru&';
-    return this.wrapObservable(this.http.get(this.url + `&lat=${lat}&lon=${lng}`));
-}
+    }
 
-wrapObservable(obs: Observable<Response>) {
-    let sharableObs = obs.share();
-    sharableObs.subscribe(
-        resp => { },
-        errResp => {
-            let messages = [];
-            let err = errResp.json();
-            messages.push(err.errorMessage);
-            this.toast.show('Service unavailable')
-           console.log(messages.join('\n'));
-        })
-    return sharableObs.map(resp => resp.json());
-}
+    getAddress(lat: number, lng: number, isLang?: boolean, isZoom?: boolean) {
+        let lang = SYS_OPTIONS.LANG_CODE;
+        if (isLang) {
+            this.url = this.url + `&accept-language=${lang}`;
+        }
+        if (isZoom) {
+            this.url = this.url + `&zoom=10`;
+        }
+        return this.wrapObservable(this.http.get(this.url + `&lat=${lat}&lon=${lng}`));
+    }
+
+    wrapObservable(obs: Observable<Response>) {
+        let sharableObs = obs.share();
+        sharableObs.subscribe(
+            resp => { },
+            errResp => {
+                let messages = [];
+                let err = errResp.json();
+                messages.push(err.errorMessage);
+                this.toast.show('Service unavailable')
+                console.log(messages.join('\n'));
+            })
+        return sharableObs.map(resp => resp.json());
+    }
 
 }
