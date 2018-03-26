4f1dc58e19d459543ec4a5979ff62f3a96c098e2
Serhii Poraiko <serhii.poraiko@toavalon.com>
2018-03-26 16:43:40 +0300

    added link popover component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -97,6 +97,7 @@ import { FavoritesService } from '../providers/favorites.service';
 import { FormatTimePipe } from '../pipes/format-time.pipe';
 import { TestimonialsService } from '../providers/testimonials.service';
 import { NoPlacesPopover } from '../pages/places/noPlaces.popover';
+import { LinkPopover } from '../pages/offer/link.popover';
 
 
 // The translate loader needs to know where to load i18n files
@@ -151,6 +152,7 @@ export function createTranslateLoader(http: HttpClient) {
         CreateAdvUserProfileFeaturesPopover,
         CreateOfferInformationPopover,
         NoPlacesPopover,
+        LinkPopover,
         OfferTermsPage,
         SettingsChangePhonePage,
         AdvRedeemOfferPage,
@@ -234,6 +236,7 @@ export function createTranslateLoader(http: HttpClient) {
         CreateAdvUserProfileFeaturesPopover,
         CreateOfferInformationPopover,
         NoPlacesPopover,
+        LinkPopover,
         OfferTermsPage,
         SettingsChangePhonePage,
         AdvRedeemOfferPage,
---
+++ src/pages/offer/link.popover.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'link-popover-component',
+    templateUrl: 'link.popover.html'
+})
+
+export class LinkPopover {
+
+    constructor() {
+
+    }
+}
