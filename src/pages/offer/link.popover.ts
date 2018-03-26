eb606cfa983d8f1e621c19d380261500db1047fc
Serhii Poraiko <serhii.poraiko@toavalon.com>
2018-03-26 20:27:01 +0300

    added link popover bindings

--- src/pages/offer/link.popover.ts
+++ src/pages/offer/link.popover.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavParams } from 'ionic-angular';
 
 @Component({
     selector: 'link-popover-component',
@@ -7,7 +8,11 @@ import { Component } from '@angular/core';
 
 export class LinkPopover {
 
-    constructor() {
+link;
 
+    constructor(
+        private navParams: NavParams) {
+
+        this.link = this.navParams.get('link');
     }
 }
