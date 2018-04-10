10b8610193d68010007759d89f716ce663608cfc
Serhii Poraiko <serhii.poraiko@toavalon.com>
2018-04-10 18:26:53 +0300

    refactored place model

--- src/models/place.ts
+++ src/models/place.ts
@@ -12,25 +12,27 @@ export class Place {
     radius: number;
     stars: number;
     is_featured: boolean;
-    is_favorite?: boolean;
+    is_favorite: boolean;
     is_starred?: boolean;
     created_at: string;
     updated_at: string;
     alias: string;
     has_active_offers: boolean;
-    testimonials_count?: number;
-    testimonials?;
+    testimonials_count: number;
+    testimonials;
     categories_count?: number;
     offers_count: number;
     active_offers_count?: number;
-    offers?: Offer[];
+    offers: Offer[];
     picture_url: string;
     cover_url: string;
     retail_types;
     category;
     tags?: string[]; 
-    timezone?: string;
-    timezone_offset?: string
+    timezone: string;
+    timezone_offset: string
+    phone: string;
+    site: string;
     specialities?;
    
 }
