d82c0e5cdeebd0160df1033593658d7233a67472
Serhii Poraiko <serhii.poraiko@toavalon.com>
2018-04-16 12:18:38 +0300

    added pushToken service methods

--- src/providers/pushToken.service.ts
+++ src/providers/pushToken.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from "@angular/core";
 import { ApiService } from "./api.service";
+import { PushTokenCreate } from "../models/pushTokenCreate";
 
 @Injectable()
 export class PushTokenService {
 
-    constructor(private api: ApiService) {
+    constructor(private api: ApiService) {}
+
+    get(deviceId) {
+        return this.api.get(`push-token/${deviceId}`, { showLoading: false });
+    }
+
+    post(pushToken: PushTokenCreate) {
+        return this.api.post('push-token', pushToken, { showLoading: false });
     }
 }
