4431a3b0d1cde4167e3e00421bed8141e6f4df2a
Serhii Poraiko <serhii.poraiko@toavalon.com>
2018-04-19 22:02:56 +0300

    fixed timeframes handler

--- src/utils/date-time.utils.ts
+++ src/utils/date-time.utils.ts
@@ -162,7 +162,11 @@ export class DateTimeUtils {
             let timeInMinutes = date.getUTCHours() * 60 + date.getUTCMinutes();
             let fromInMinutes = parseInt(day.from.split(':')[0]) * 60 + parseInt(day.from.split(':')[1].slice(0, 2));
             let toInMinutes = parseInt(day.to.split(':')[0]) * 60 + parseInt(day.to.split(':')[1].slice(0, 2));
-            isIncluded = timeInMinutes > fromInMinutes && timeInMinutes < toInMinutes;
+            if ((fromInMinutes < toInMinutes && timeInMinutes >= fromInMinutes && timeInMinutes <= toInMinutes) 
+            || (fromInMinutes > toInMinutes)) {
+                isIncluded = true;
+            }
+            else isIncluded = false;
         }
         timeFrames = timeFrames.map(item => {
             return {
