3bc9f61fc1dea7feec5c8663fd915047a1397690
Serhii Poraiko <serhii.poraiko@toavalon.com>
2018-06-19 23:32:03 +0300

    changed adjust environment mode

--- src/providers/adjust.service.ts
+++ src/providers/adjust.service.ts
@@ -59,8 +59,8 @@ export class AdjustService {
             // else if (this.platform.is('ios')) {
             //     appToken = this.ADJUST_IOS_APP_TOKEN;
             // }
-            // let adjustConfig = new AdjustConfig(appToken, AdjustConfig.EnvironmentProduction); //for prod
-            let adjustConfig = new AdjustConfig(appToken, AdjustConfig.EnvironmentSandbox); //for test
+            let adjustConfig = new AdjustConfig(appToken, AdjustConfig.EnvironmentProduction); //for prod
+            // let adjustConfig = new AdjustConfig(appToken, AdjustConfig.EnvironmentSandbox); //for test
 
             // adjustConfig.setAttributionCallbackListener(function (attribution) {
             //     // Printing all attribution properties.
