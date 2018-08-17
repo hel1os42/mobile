e1fc79986753655ed23849160a2c1e89e3fbbed4
Serhii Poraiko <serhii.poraiko@toavalon.com>
2018-08-17 18:01:24 +0300

    formatted create-offer pages

--- src/pages/create-offer-5/create-offer-5.ts
+++ src/pages/create-offer-5/create-offer-5.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { LoadingController, NavController, NavParams } from 'ionic-angular';
-import { AlertController } from 'ionic-angular';
+import { AlertController, LoadingController, NavController, NavParams } from 'ionic-angular';
 import { Offer } from '../../models/offer';
 import { ApiService } from '../../providers/api.service';
 import { PlaceService } from '../../providers/place.service';
@@ -8,11 +7,13 @@ import { ProfileService } from '../../providers/profile.service';
 import { ToastService } from '../../providers/toast.service';
 import { StringValidator } from '../../validators/string.validator';
 
-
 @Component({
     selector: 'page-create-offer-5',
     templateUrl: 'create-offer-5.html'
 })
+
+// this page is not used
+
 export class CreateOffer5Page {
 
     offer: Offer;
@@ -33,6 +34,7 @@ export class CreateOffer5Page {
 
         this.offer = this.navParams.get('offer');
         this.picture_url = this.navParams.get('picture');
+
         if (this.offer.id) {
             this.reward = this.offer.reward.toString();
             this.reserved = this.offer.reserved.toString();
@@ -44,58 +46,60 @@ export class CreateOffer5Page {
 
     createOffer() {
         if (this.validate()) {
-        this.offer.reward = parseInt(this.reward);
-        this.offer.reserved = parseInt(this.reserved);
-        this.place.setOffer(this.offer)
-            .subscribe(resp => {
-                let location = resp.http_headers.get('location');
-                // let offer_id = location.slice(- location.lastIndexOf('/') + 2);
-                let offer_id = location.split('offers/')[1];
-                let loading = this.loading.create({ content: 'Creating your offer...', spinner: 'bubbles' });
-                loading.present();
-                this.timer = setInterval(() => {
+            this.offer.reward = parseInt(this.reward);
+            this.offer.reserved = parseInt(this.reserved);
+            this.place.setOffer(this.offer)
+                .subscribe(resp => {
+                    let location = resp.http_headers.get('location');
+                    // let offer_id = location.slice(- location.lastIndexOf('/') + 2);
+                    let offer_id = location.split('offers/')[1];
+                    let loading = this.loading.create({ content: 'Creating your offer...', spinner: 'bubbles' });
+                    loading.present();
+                    this.timer = setInterval(() => {
 
-                    this.place.getOffer(offer_id)
-                        .subscribe(offer => {
-                            if (offer.id) {
-                                this.place.refreshPlace();
+                        this.place.getOffer(offer_id)
+                            .subscribe(offer => {
 
-                                let promise = this.picture_url
-                                    ? this.api.uploadImage(this.picture_url, `offers/${offer_id}/picture`, false)
-                                    : Promise.resolve();
+                                if (offer.id) {
+                                    this.place.refreshPlace();
 
-                                promise.then((res) => {
-                                    console.log(res);
-                                    this.stopTimer();
-                                    loading.dismiss();
-                                    this.navTo();
-                                }).catch(t => console.log(t.error()));
-                            }
-                        });
-                }, 1500)
-            }, err => this.presentConfirm('created'))
+                                    let promise = this.picture_url
+                                        ? this.api.uploadImage(this.picture_url, `offers/${offer_id}/picture`, false)
+                                        : Promise.resolve();
+
+                                    promise.then((res) => {
+                                        console.log(res);
+                                        this.stopTimer();
+                                        loading.dismiss();
+                                        this.navTo();
+                                    }).catch(t => console.log(t.error()));
+                                }
+
+                            });
+                    }, 1500)
+                }, err => this.presentConfirm('created'))
         }
     }
 
     updateOffer() {
         if (this.validate()) {
-        this.offer.reward = parseInt(this.reward);
-        this.offer.reserved = parseInt(this.reserved);
-        this.place.putOffer(this.offer, this.offer.id)
-            .subscribe(() => {
-                let loading = this.loading.create({ content: 'Updating offer...', spinner: 'bubbles' });
-                loading.present();
-                let promise = this.picture_url
-                    ? this.api.uploadImage(this.picture_url, `offers/${this.offer.id}/picture`, false)
-                    : Promise.resolve();
+            this.offer.reward = parseInt(this.reward);
+            this.offer.reserved = parseInt(this.reserved);
+            this.place.putOffer(this.offer, this.offer.id)
+                .subscribe(() => {
+                    let loading = this.loading.create({ content: 'Updating offer...', spinner: 'bubbles' });
+                    loading.present();
+                    let promise = this.picture_url
+                        ? this.api.uploadImage(this.picture_url, `offers/${this.offer.id}/picture`, false)
+                        : Promise.resolve();
 
-                promise.then(() => {
-                    loading.dismiss();
-                    this.navTo();
-                })
-            },
-            err => this.presentConfirm('updated')
-            );
+                    promise.then(() => {
+                        loading.dismiss();
+                        this.navTo();
+                    })
+                },
+                    err => this.presentConfirm('updated')
+                );
         }
     }
 
@@ -109,8 +113,8 @@ export class CreateOffer5Page {
     presentConfirm(action: string) {
         let isUpdate = action == 'updated' ? true : false;
         let subTitle = isUpdate ?
-        `Offer wasn't update. Please try again` :
-        `Offer wasn't create. Please try again`;
+            `Offer wasn't update. Please try again` :
+            `Offer wasn't create. Please try again`;
         let alert = this.alert.create({
             title: 'Oops...ERROR',
             subTitle: subTitle,
@@ -168,10 +172,7 @@ export class CreateOffer5Page {
     }
 
     disable() {
-        return  parseInt(this.reserved) <= 0 || parseInt(this.reward) <= 0 || this.reserved == '' || this.reward == '';
+        return parseInt(this.reserved) <= 0 || parseInt(this.reward) <= 0 || this.reserved == '' || this.reward == '';
     }
 
-    close() {
-
-    }
 }
